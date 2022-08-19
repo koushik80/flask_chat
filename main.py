c98ef88534e8d6a06a4f6a521406cc34166d8be7
koushik <koushik.dey@live.com>
2022-08-19 16:06:49 +0300

    socket

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-import streamlit as st
+#Back-end (web-server)
+
+#import streamlit as st
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 #from flask_cors import CORS
 
 app = Flask(__name__)
-#socketio = SocketIO(app, cors_allowed_origins="*")
+app.config['SECRET'] = "secret!"
+socketio = SocketIO(app, cors_allowed_origins="*")
+# cors_allowed_origins = list of origins authorized to make requests
+
+
+@socketio.on('message')
+def handle_message(msg):
+    print(msg)
+    if msg == 'save_to_db':
+        save_to_db()
+        send(msg, broadcast=True)
 
 
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def index():
+    return render_template("index.html")
+
+
+def save_to_db():
+    print("SAVING TO DB...")
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='localhost',)
