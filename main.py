da40791a77560ae2dbcb23bc77b50f352b6f98bb
koushik <koushik.dey@live.com>
2022-08-29 09:37:15 +0300

    viestit

--- main.py
+++ main.py
@@ -3,20 +3,17 @@
 #import streamlit as st
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
-#from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SECRET'] = "secret!"
+# CORS_ALLOWED_ORIGINS is the list of origins authorized to make requests
 socketio = SocketIO(app, cors_allowed_origins="*")
-# cors_allowed_origins = list of origins authorized to make requests
 
 
-@socketio.on('message')
+@socketio.on('message')  # pitää olla message
 def handle_message(msg):
-    print(msg)
-    if msg == 'save_to_db':
-        save_to_db()
-        send(msg, broadcast=True)
+    if msg == 'get_messages':
+        print("serveri sai viestin get_messages.")
+        # haetaan tiedostosta viestit ja lähetetään ne clientille.
 
 
 @app.route('/')
@@ -24,9 +21,9 @@ def index():
     return render_template("index.html")
 
 
-def save_to_db():
-    print("SAVING TO DB...")
+if __name__ == "__main__":
+    socketio.run(app, host="localhost")
 
 
-if __name__ == '__main__':
-    socketio.run(app, host='localhost',)
+# google cdn socket.io
+# https://cdnjs.com/libraries/socket.io
